12afb56c5af5e2f4ca63b0b4c12bd0c57510a520
Paul <38015981+paul-nadan@users.noreply.github.com>
2024-11-05 18:07:11 -0500

    Select Wi-Fi network name based on Arduino serial number

--- MiniRHex/wireless.ino
+++ MiniRHex/wireless.ino
@@ -6,7 +6,6 @@
 #include <WiFiNINA.h>
 #include <utility/wifi_drv.h>
 
-char ssid[] = "minirhex";
 WiFiServer server(80);
 const int RED = 25;
 const int GREEN = 26;
@@ -28,7 +27,7 @@ void begin_wifi() {
     Serial.println("Please upgrade the WiFi firmware");
   }
   WiFi.config(IPAddress(10, 0, 0, 1));
-  if (WiFi.beginAP(ssid) != WL_AP_LISTENING) {
+  if (WiFi.beginAP(get_ssid().c_str()) != WL_AP_LISTENING) {
     Serial.println("Creating WiFi network failed");
     return;
   }
@@ -114,4 +113,32 @@ void display_website(WiFiClient client) {
 
   // Terminate http response
   client.println();
+}
+
+String get_ssid() {
+  int id = get_id();
+  switch(id) {
+    case 0x0A0A: return "GANDALF";
+    case 0x2245: return "IRIS";
+    case 0x2238: return "PUMPKIN";
+    case 0x322D: return "GOLDILOCKS";
+    case 0x0A2B: return "SHADOW";
+    case 0x1518: return "ZEBRA";
+    // Add your robot's name here!
+    // You can find the last 4 digits of the SN under Tools > Get Board Info,
+    // or just look at the default name of the robot's Wi-Fi network
+    default:
+      String ssid = String(id, HEX);
+      while (ssid.length() < 4) ssid = "0" + ssid;
+      ssid.toUpperCase();
+      return "MINIRHEX-" + ssid;
+  }
+}
+
+int get_id() {
+  #define SERIAL_NUMBER_WORD_0  *(volatile uint32_t*)(0x0080A00C)
+  #define SERIAL_NUMBER_WORD_1  *(volatile uint32_t*)(0x0080A040)
+  #define SERIAL_NUMBER_WORD_2  *(volatile uint32_t*)(0x0080A044)
+  #define SERIAL_NUMBER_WORD_3  *(volatile uint32_t*)(0x0080A048)
+  return SERIAL_NUMBER_WORD_3 & 0xFFFF;  // Last 4 hexadecimal digits of SN
 }
